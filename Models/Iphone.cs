1c69260686bb74cd482624f1ee4576d101d2c856
Rafael Land <rafael.land@hotmail.com>
2023-12-31 00:06:45 -0300

    Desafio Concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
-namespace DesafioPOO.Models
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioTrilhaNetPoo.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o mÃ©todo "InstalarAplicativo"
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na App Store do iPhone...");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,22 @@
-namespace DesafioPOO.Models
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioTrilhaNetPoo.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na loja da Nokia...");
+        }
     }
 }
